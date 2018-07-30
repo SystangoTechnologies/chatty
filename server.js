cd91ca8498708cf12fb8ab502ef8a53086834ec8
Anurag Singh <anurag@systango.com>
2018-07-30 15:43:00 +0530

    Added static view

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-'use strict'
+// 'use strict'
 
 // Chat application dependencies
 var express = require('express')
 var app = express()
 var ioServer = require('./app/socket')(app)
+var path = require('path');
+var public = path.join(__dirname, 'public');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public, 'index.html'));
+});
+
+app.use('/', express.static(public));
+
 
 // Set the port number
-var port = process.env.PORT || 6001
+var port = process.env.PORT || 6003
 ioServer.listen(port)
