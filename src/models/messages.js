d53babf364c7cae2f846497c8de8b33e604434ca
Your Name <you@example.com>
2018-08-20 21:21:24 +0530

    reverted  code

--- src/models/messages.js
+++ src/models/messages.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false
         },
         data: {
-            type: Sequelize.JSON
+            type: Sequelize.STRING
         },
         sender: {
             type: Sequelize.STRING
